d5d852e7090a44ae10dc76e0147b3cd729060150
v-d-k <117153816+v-d-k@users.noreply.github.com>
2023-07-30 11:57:12 +0530

    update contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,7 @@
-import Counter from "./Counter"
-
 export default function About() {
     return (
         <>
         <h1>Hello from contact</h1>
-            <Counter/>
         <p>contact page</p>
         </>
     )
